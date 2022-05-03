39ba953dec33ce83792955bdeff9cb120d36f94a
HSMughal <hshahmughal@gmail.com>
2022-05-03 18:37:54 -0400

    Update justplot.py

--- justplot.py
+++ justplot.py
@@ -76,4 +76,4 @@ lime_explanation = lime_explainer.explain_instance(data_row = X_test.iloc[2],
                                          num_features = 3, 
                                          num_samples = 20)
 
-components.html(lime_explaination.as_html())
+components.html(lime_explanation.as_html())
